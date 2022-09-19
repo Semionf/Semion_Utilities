b70c701b9372d34e8a30fd7896993eb027a87d8c
semionf <sfurlender@gmail.com>
2022-09-19 12:20:10 +0300

    .

--- Semion_Utilities/Utilities.c
+++ Semion_Utilities/Utilities.c
@@ -3,7 +3,14 @@
 
 #pragma warning(disable:4996)
 
+char FILE_NAME[1000];
 
+void Log_Init(char fileName[])
+{
+	strcpy(FILE_NAME, fileName);
+	FILE* f = fopen(fileName, "w");
+	fclose(f);
+}
 void Log(char str[])
 {
 	char Log[100];
@@ -13,7 +20,7 @@ void Log(char str[])
 	struct tm* timeInfo;
 	timeInfo = localtime(&t);
 	sprintf(Log, "%d %d %d: %02d:%02d:%02d- %s", timeInfo->tm_year + 1900, timeInfo->tm_mon + 1, timeInfo->tm_mday, timeInfo->tm_hour, timeInfo->tm_min, timeInfo->tm_sec, str);
-	FILE* f = fopen("myProg.log", "a");
+	FILE* f = fopen(FILE_NAME, "a");
 	if (!f)
 	{
 		printf("File Error");
